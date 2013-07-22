69a441d6709475b2fcefc3b7c71dd2afc2c79490
Patrik Hulthén <patrik.hulthen@gmail.com>
2013-07-22 17:34:20 +0200

    Class FirstTestHui added.

---
+++ HuiGUI/src/HuiCert/FirstTestHui.java
@@ -0,0 +1,18 @@
+package HuiCert;
+
+public class FirstTestHui {
+
+	public FirstTestHui() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Hui!");
+
+	}
+
+}
