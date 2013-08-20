00c5eb2bbb78dda7da9b953eec5fb9f46a91a30e
Patrik Hulthén <patrik.hulthen@live.com>
2013-08-21 01:00:25 +0200

    New println, jar-file, ant-buid xml

--- FirstTest/src/firstTest/HelloWorld1.java
+++ FirstTest/src/firstTest/HelloWorld1.java
@@ -22,6 +22,7 @@ public class HelloWorld1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World 1");
+		System.out.println("Hello again");
 		
 		Class1 class1 = new Class1();
 		class1.message1();
